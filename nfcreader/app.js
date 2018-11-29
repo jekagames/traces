a950d65415618fe9891848671b76b1a32db99aaa
Jess Marcotte <meaikoh@gmail.com>
2018-11-28 20:26:14 -0500

    Not so broken code!
    The current goal is to pass values from the arduino serial port through
    to the app and html index page.
    
    The problem is that serialport has changed a lot since most of the
    example code was written, so I'm trying to use the correct
    documentation.

--- nfcreader/app.js
+++ nfcreader/app.js
@@ -1,10 +1,36 @@
 const SerialPort = require('serialport');
-const NFCport = new SerialPort('/COM8', function (err) {
+const NFCport = new SerialPort('COM8', function (err) {
+  baudrate: 115200;
   if (err) {
     return console.log('Error: ', err.message)
   }
+
 });
 
-NFCport.on('currentStory', function (data) {
+NFCport.on('open', function (data) {
   console.log('currentStory', data);
+  socket.emit('currentStory', data);
 });
+
+const Readline = require('@serialport/parser-readline')
+const parser = port.pipe(new Readline({ delimiter: '\n' }))
+NFCport.pipe(parser);
+parser.on('data', function (data) {
+  console.log('Timestream data received' + data);
+});
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+
+socket.on('connection', function(socket)
+{
+	console.log('Connecting to the stream of time.');
+});
+
+http.listen(8080, function()
+{
+	console.log('Open localhost:8080 in your web browser!');
+});
+
