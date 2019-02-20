45528784672eff8088b472160e3627ca6b139688
Jess Marcotte <meaikoh@gmail.com>
2019-02-19 22:23:45 -0500

    THE SCREEN WORKS!
    I have the data sending to the LCD screen.
    
    Now I need to figure out how to reprogram the EEPROM/permanent memory of
    the LCD so that:
    1) it detects as a 20x4 screen (currently set to 16x2)
    2) I want to mess with the screen colours

--- nfcreader/send.js
+++ nfcreader/send.js
@@ -1,6 +1,6 @@
 var currentStory = "Story 0";
 const SerialPort = require('serialport');
-const Lcd = require('lcd');
+//const Lcd = require('lcd');
 //COM PORT has to be read and changed accordingly in windows from the Arduino IDE. Maybe there's a way to make the code auto-select the correct port? 
 //Or maybe this will just be easier in Linux
 const NFCport = new SerialPort('COM7', {
@@ -8,11 +8,11 @@ const NFCport = new SerialPort('COM7', {
   }
 );
 
-const lcd = new Lcd('COM9', {rs: 12, e: 11, data: [5, 4, 3, 2], cols: 20, rows: 4, baudRate: 115200});
-// const screenPort = new SerialPort('COM9', {
-//   baudRate: 115200
-//   }
-// );
+//const lcd = new Lcd('COM9', {rs: 12, e: 11, data: [5, 4, 3, 2], cols: 20, rows: 4, baudRate: 115200});
+const screenPort = new SerialPort('COM9', {
+  baudRate: 115200
+  }
+);
 
 NFCport.on('data', function (data) {
   //console.log(data);
@@ -30,19 +30,26 @@ parser.on('data', function (data) {
   //console.log(data);
   currentStory = data;
   console.log(currentStory);
+  //can I nest these?
+screenPort.on('data', function(data) {
+  socket.emit('data', currentStory)
+})
+screenPort.write('Hello');
   //console.log("");
 });
 
-lcd.on('data', function(data) {
-console.log("hello!");
-}
-)
+
+
+
+//lcd.on('data', function(data) {
+//console.log("hello!");
+//}
+//)
 
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var socket = require('socket.io')(http);
-var bindings = require('bindings')('binding.node')
 
 app.get('/', function(req, res)
 {
@@ -68,74 +75,74 @@ http.listen(8080, function()
 //Managing text and audio through a spreadsheet (json dictionary)
 //Make the spreadsheet then parse it into a json dictionary using Mr Data Converter or other service -- 
 //https://shancarter.github.io/mr-data-converter/)
-var database = {
-  "1635431931": { "title":"story001", "text":"[You can see their traces permeating the space, tying echoes of their very beings to the artefacts that they left behind, to the spaces and things that meant something to them here. Track down the artefacts and spaces, and we can analyze the traces. That's what you're here to]", "audio_filename":"log001.ogg"}
-}
+// var database = {
+//   "1635431931": { "title":"story001", "text":"[You can see their traces permeating the space, tying echoes of their very beings to the artefacts that they left behind, to the spaces and things that meant something to them here. Track down the artefacts and spaces, and we can analyze the traces. That's what you're here to]", "audio_filename":"log001.ogg"}
+// }
 
-var lineMaxCharacters = 20;
-var maxRows = 4;
-var delayPerChunk = 1000;
+// var lineMaxCharacters = 20;
+// var maxRows = 4;
+// var delayPerChunk = 1000;
 
-console.log("Initial info:")
-console.log(database);
+// console.log("Initial info:")
+// console.log(database);
 
-function getStoryLine(raw_id){
-  return database[raw_id.toString()];
-}
+// function getStoryLine(raw_id){
+//   return database[raw_id.toString()];
+// }
 
-var testStoryLine = getStoryLine("121928132");
+// var testStoryLine = getStoryLine("121928132");
 
-console.log(testStoryLine);
-console.log(processRawText(testStoryLine));
-displayStoryNode(testStoryLine);
+// console.log(testStoryLine);
+// console.log(processRawText(testStoryLine));
+// displayStoryNode(testStoryLine);
 
-// this expects a node with title, and rawtext, with chunks empty
-function processRawText(storyline){
-  var rawText = storyline.text;
-  var words = rawText.split(" ");
-  var rows = [];
-  var currentRow = "";
+// // this expects a node with title, and rawtext, with chunks empty
+// function processRawText(storyline){
+//   var rawText = storyline.text;
+//   var words = rawText.split(" ");
+//   var rows = [];
+//   var currentRow = "";
 
-  console.log(words);
+//   console.log(words);
 
-  for (i = 0; i < words.length; i++){
-    if(currentRow.length + words[i].length > lineMaxCharacters){
-      // we need to split
-      // add current row to array
-      rows.push(currentRow.trim());
-      // create a new row
-      currentRow = words[i] + " ";
-    }
-    else {
-      currentRow += words[i] + " ";
-    }
-  }
-  rows.push(currentRow.trim());
-  storyline.rows = rows;
-  return storyline;
-}
+//   for (i = 0; i < words.length; i++){
+//     if(currentRow.length + words[i].length > lineMaxCharacters){
+//       // we need to split
+//       // add current row to array
+//       rows.push(currentRow.trim());
+//       // create a new row
+//       currentRow = words[i] + " ";
+//     }
+//     else {
+//       currentRow += words[i] + " ";
+//     }
+//   }
+//   rows.push(currentRow.trim());
+//   storyline.rows = rows;
+//   return storyline;
+// }
 
-function displayStoryNode(storynode){
-  // sends the text to serialport 4 rowas at a time
-  var lastIndex =-1;
-  var chunks = [];
-  for(i = 0; i < storynode.rows.length; i++) {
-    var msg = "";
-    var chunk = storynode.rows.slice(i, i + 4);
-    msg = chunk.join(" ");
-    lastIndex = i+3;
-    chunks.push(msg);
-  }
-  for (d = 0; d < chunks.length; d++){
-    var display = chunks[d];
-    setTimeout(displaySingleChunk, delayPerChunk*(d+1), display);
-  }
-}
+// function displayStoryNode(storynode){
+//   // sends the text to serialport 4 rowas at a time
+//   var lastIndex =-1;
+//   var chunks = [];
+//   for(i = 0; i < storynode.rows.length; i++) {
+//     var msg = "";
+//     var chunk = storynode.rows.slice(i, i + 4);
+//     msg = chunk.join(" ");
+//     lastIndex = i+3;
+//     chunks.push(msg);
+//   }
+//   for (d = 0; d < chunks.length; d++){
+//     var display = chunks[d];
+//     setTimeout(displaySingleChunk, delayPerChunk*(d+1), display);
+//   }
+// }
 
-function displaySingleChunk(chunk){
-  //instead of logging to console, emit to the serial port 
-  console.log(chunk);
-}
+// function displaySingleChunk(chunk){
+//   //instead of logging to console, emit to the serial port 
+//   console.log(chunk);
+// }
 
 
 
