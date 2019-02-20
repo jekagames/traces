8bd4271e786902710bb0d3c154fba72467b78f97
Jess Marcotte <meaikoh@gmail.com>
2019-02-20 17:19:40 -0500

    Current task: Implementing Text database...
    Still working on integrating that code and improving the work flow for
    what I have now for audio as well.

--- nfcreader/receive.js
+++ nfcreader/receive.js
@@ -1,18 +1,21 @@
 //the receive code for telling the web page how to handle all this information
-var receivedStory;
+var receivedSound;
+var receivedTextCue;
 var soundInstance;
 
 var socket = io();
 			socket.on('data', function(data)
-			{
-				var newStory = data.trim();
-				//if (data.match(/[a-z]/i) && data != receivedStory) 
+			{ 
+				var newStory = data.trim(); 
 				if (newStory != "") 
 				{
+					receivedTextCue = newStory;
+					console.log("Text Reference" + receivedTextCue);
+					
 					if (!(soundInstance && soundInstance.playState != createjs.Sound.PLAY_FINISHED))
 					{
-					receivedStory = newStory;
-					console.log(receivedStory);
+					receivedSound = newStory;
+					console.log("Audio Reference" + receivedSound);
 					storyTeller();
 					}
 				}
@@ -21,13 +24,22 @@ var socket = io();
 			createjs.Sound.registerSound("log001.ogg", "log001");
 			createjs.Sound.registerSound("log002.ogg", "log002");
 
-function storyTeller() {
-if (receivedStory == "story001") 
+//DATABASE TO READ STORY AND AUDIO FILE DIRECTIONS FROM
+//Managing text and audio through a spreadsheet (json dictionary)
+//Make the spreadsheet then parse it into a json dictionary using Mr Data Converter or other service -- 
+//https://shancarter.github.io/mr-data-converter/)
+var database = {
+  "1635431931": { "title":"story001", "text":"[You can see their traces permeating the space, tying echoes of their very beings to the artefacts that they left behind, to the spaces and things that meant something to them here. Track down the artefacts and spaces, and we can analyze the traces. That's what you're here to]", "audio_filename":"log001.ogg"}
+}
+
+//PLAY AUDIO
+function storyAudio() {
+if (receivedSound == "1635431931") 
 {
 //play sound log001
 soundInstance = createjs.Sound.play("log001");
 }
-else if (receivedStory == "story002")
+else if (receivedSound == "17938683")
 {
 	//play sound log002
 soundInstance = createjs.Sound.play("log002");
@@ -35,7 +47,77 @@ soundInstance = createjs.Sound.play("log002");
 
 else
 {
-	console.log("No story matches");
+	console.log("I can't identify this trace.");
 }
 
 			}
+
+//ENRIC's awesome LCD text scroller
+
+var lineMaxCharacters = 20;
+var maxRows = 4;
+var delayPerChunk = 1000;
+
+console.log("Searching for traces...")
+console.log(database);
+
+function getStoryLine(receivedTextCue){
+  return database[receivedTextCue.tostring()];
+  //
+}
+
+var testStoryLine = getStoryLine();
+
+console.log(testStoryLine);
+console.log(processRawText(testStoryLine));
+displayStoryNode(testStoryLine);
+
+// this expects a node with title, and rawtext, with chunks empty
+function processRawText(receivedTextCue){
+  var rawText = receivedTextCue.text;
+  var words = rawText.split(" ");
+  var rows = [];
+  var currentRow = "";
+
+  console.log(words);
+
+  for (i = 0; i < words.length; i++){
+    if(currentRow.length + words[i].length > lineMaxCharacters){
+      // we need to split
+      // add current row to array
+      rows.push(currentRow.trim());
+      // create a new row
+      currentRow = words[i] + " ";
+    }
+    else {
+      currentRow += words[i] + " ";
+    }
+  }
+  rows.push(currentRow.trim());
+  storyline.rows = rows;
+  return storyline;
+}
+
+function displayStoryNode(storynode){
+  // sends the text to serialport 4 rowas at a time
+  var lastIndex =-1;
+  var chunks = [];
+  for (i = 0; i < storynode.rows.length; i++) {
+    var msg = "";
+    var chunk = storynode.rows.slice(i, i + 4);
+    msg = chunk.join(" ");
+    lastIndex = i+3;
+    chunks.push(msg);
+  }
+  for (d = 0; d < chunks.length; d++){
+    var display = chunks[d];
+    setTimeout(displaySingleChunk, delayPerChunk*(d+1), display);
+//Trying to fulfill that promise!
+setTimeout.then(console.log("Analyzing trace."), console.log("This trace is unreadable."));
+  }
+}
+
+function displaySingleChunk(chunk){
+  //instead of logging to console, emit to the serial port 
+  screenPort.write(chunk);
+}
--- nfcreader/send.js
+++ nfcreader/send.js
@@ -1,8 +1,7 @@
-var currentStory = "Story 0";
+var currentStory = "";
 const SerialPort = require('serialport');
-//const Lcd = require('lcd');
-//COM PORT has to be read and changed accordingly in windows from the Arduino IDE. Maybe there's a way to make the code auto-select the correct port? 
-//Or maybe this will just be easier in Linux
+
+//COM PORT has to be read and changed accordingly in windows. It can be read in the Arduino IDE. 
 const NFCport = new SerialPort('COM7', {
   baudRate: 115200
   }
@@ -14,7 +13,7 @@ const screenPort = new SerialPort('COM9', {
   }
 );
 
-NFCport.on('data', function (data) {
+NFCport.once('data', function (data) {
   //console.log(data);
   //socket.emit('data', data);
   socket.emit('data', currentStory);
@@ -26,15 +25,18 @@ const parser = NFCport.pipe(new Readline());
 NFCport.pipe(parser);
 //console.log(NFCport);
 parser.on('data', function (data) {
-	//console.log("Analyzing traces...");
-  //console.log(data);
-  currentStory = data;
+	console.log("Analyzing traces...");
+  console.log(data);
+  currentStory = data.trim();
   console.log(currentStory);
   //can I nest these?
-screenPort.on('data', function(data) {
-  socket.emit('data', currentStory)
+  screenPort.on('data', function(data) {
+  socket.emit('data');
+  currentStory = data.trim();
 })
-screenPort.write('Hello');
+screenPort.write("The current card is " + currentStory + " ");
+console.log(currentStory);
+
 
 });
 
@@ -54,88 +56,13 @@ app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/libraries'));
 app.use(express.static(__dirname + '/assets'));
 
-socket.on('connection', function(socket)
+socket.once('connection', function(socket)
 {
 	console.log('Connecting to the stream of time.');
 });
 
 http.listen(8080, function()
 {
+
 	console.log('Open localhost:8080 in your web browser!');
 });
-
-//ENRIC's TEXT SCROLLING CODE
-//Managing text and audio through a spreadsheet (json dictionary)
-//Make the spreadsheet then parse it into a json dictionary using Mr Data Converter or other service -- 
-//https://shancarter.github.io/mr-data-converter/)
-var database = {
-  "1635431931": { "title":"story001", "text":"[You can see their traces permeating the space, tying echoes of their very beings to the artefacts that they left behind, to the spaces and things that meant something to them here. Track down the artefacts and spaces, and we can analyze the traces. That's what you're here to]", "audio_filename":"log001.ogg"}
-}
-
-var lineMaxCharacters = 20;
-var maxRows = 4;
-var delayPerChunk = 1000;
-
-console.log("Initial info:")
-console.log(database);
-
-function getStoryLine(raw_id){
-  return database[raw_id.toString()];
-}
-
-var testStoryLine = getStoryLine("121928132");
-
-console.log(testStoryLine);
-console.log(processRawText(testStoryLine));
-displayStoryNode(testStoryLine);
-
-// this expects a node with title, and rawtext, with chunks empty
-function processRawText(storyline){
-  var rawText = storyline.text;
-  var words = rawText.split(" ");
-  var rows = [];
-  var currentRow = "";
-
-  console.log(words);
-
-  for (i = 0; i < words.length; i++){
-    if(currentRow.length + words[i].length > lineMaxCharacters){
-      // we need to split
-      // add current row to array
-      rows.push(currentRow.trim());
-      // create a new row
-      currentRow = words[i] + " ";
-    }
-    else {
-      currentRow += words[i] + " ";
-    }
-  }
-  rows.push(currentRow.trim());
-  storyline.rows = rows;
-  return storyline;
-}
-
-function displayStoryNode(storynode){
-  // sends the text to serialport 4 rowas at a time
-  var lastIndex =-1;
-  var chunks = [];
-  for(i = 0; i < storynode.rows.length; i++) {
-    var msg = "";
-    var chunk = storynode.rows.slice(i, i + 4);
-    msg = chunk.join(" ");
-    lastIndex = i+3;
-    chunks.push(msg);
-  }
-  for (d = 0; d < chunks.length; d++){
-    var display = chunks[d];
-    setTimeout(displaySingleChunk, delayPerChunk*(d+1), display);
-  }
-}
-
-function displaySingleChunk(chunk){
-  //instead of logging to console, emit to the serial port 
-  console.log(chunk);
-}
-
-
-
